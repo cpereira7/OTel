0ac81935be55a2f67481ae4434c265ed06296754
cpereira7 <cpereira7@users.noreply.github.com>
2025-02-11 23:57:32 +0000

    feat(Telemetry): Moved DI registration to ServiceCollectionExtensions
    - Moved Dependency Injection configuration to the ServiceCollectionExtensions

---
+++ SampleStack.Telemetry/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SampleStack.Telemetry.HttpHandler;
+using SampleStack.Telemetry.Telemetry;
+
+namespace SampleStack.Telemetry.Configuration
+{
+    internal static class ServiceCollectionExtensions
+    {
+        public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<CustomHttpHandler>();
+
+            var httpClientConfiguration = new Action<HttpClient>(c =>
+            {
+                var apiAddress = Environment.GetEnvironmentVariable("API_HOST") ?? "localhost";
+                var apiPort = Environment.GetEnvironmentVariable("API_HTTP_PORT") ?? "8080";
+
+                c.BaseAddress = new Uri($"http://{apiAddress}:{apiPort}");
+                c.Timeout = TimeSpan.FromSeconds(60);
+            });
+
+            var httpClientHandlerConfiguration = new HttpClientHandler
+            {
+                // Ignore Server Certificate Validation
+                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+            };
+
+            services.AddHttpClient("api", httpClientConfiguration)
+                .AddHttpMessageHandler<CustomHttpHandler>()
+                .ConfigurePrimaryHttpMessageHandler(() => httpClientHandlerConfiguration);
+
+            services.ConfigureOpenTelemetryTraces(configuration);
+        }
+    }
+}
--- SampleStack.Telemetry/Program.cs
+++ SampleStack.Telemetry/Program.cs
@@ -1,66 +1,56 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using OpenTelemetry.Trace;
 using SampleStack.Telemetry.Configuration;
 using SampleStack.Telemetry.Diagnostic;
-using SampleStack.Telemetry.HttpHandler;
-using SampleStack.Telemetry.Telemetry;
-using Serilog;
-
-Console.WriteLine("Hello, World!");
 
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureAppLogging()
     .ConfigureServices((context, services) =>
     {
-        services.AddScoped<CustomHttpHandler>();
-
-        var httpClientConfiguration = new Action<HttpClient>(c =>
-        {
-            var apiAdress = Environment.GetEnvironmentVariable($"API_HOST") ?? "localhost";
-            var apiPort = Environment.GetEnvironmentVariable($"API_HTTP_PORT") ?? "8080";
-
-            c.BaseAddress = new Uri($"http://{apiAdress}:{apiPort}");
-            c.Timeout = TimeSpan.FromSeconds(60);
-        });
-
-        var httpClientHandlerConfiguration = new HttpClientHandler
-        {
-            // Ignore Server Certificate Validation
-            ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; }
-        };
-
-        services.AddHttpClient("api", httpClientConfiguration)
-            .AddHttpMessageHandler<CustomHttpHandler>()
-            .ConfigurePrimaryHttpMessageHandler(() => httpClientHandlerConfiguration);
-
-        services.ConfigureOpenTelemetryTraces(context.Configuration);
+        services.AddApplicationServices(context.Configuration);
     })
     .Build();
 
 var httpClient = host.Services.GetRequiredService<IHttpClientFactory>().CreateClient("api");
 var logger = host.Services.GetRequiredService<ILogger<Program>>();
+var configuration = host.Services.GetRequiredService<IConfiguration>();
+
 host.Services.GetRequiredService<TracerProvider>();
 
-logger.LogInformation("Starting application");
+logger.LogInformation("Starting Client Application");
 
-Console.WriteLine("Calling API");
+Console.WriteLine($"Base Address: {httpClient.BaseAddress}");
+Console.WriteLine($"Telemetry Address: {configuration.GetConnectionString("OpenTelemetry")}");
 
-while (true)
+await RunApiCallsAsync(httpClient, logger);
+
+static async Task RunApiCallsAsync(HttpClient httpClient, ILogger logger)
 {
+    for (int run = 0; run < 15; run++)
+    {
+        try
+        {
+            using var activity = DiagnosticActivity.StartActivity("Calling API", run);
+            activity?.SetTag("run", run);
 
-    _ = DiagnosticActivity.StartActivity("Calling API");
+            await httpClient.GetAsync("/weatherforecast");
 
-    var response = await httpClient.GetAsync("/weatherforecast");
-
-    var responseContent = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(response.Headers);
-    Console.WriteLine(responseContent);
-
-    Console.WriteLine("Calling 404");
-
-    _ = await httpClient.GetAsync("/weatherforecast/null");
-
-    Thread.Sleep(5000);
+            if (run % 2 == 0)
+            {
+                Console.WriteLine("Calling 404");
+                await httpClient.GetAsync("/weatherforecast/null");
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error calling API");
+        }
+        finally
+        {
+            await Task.Delay(15000);
+        }
+    }
 }
