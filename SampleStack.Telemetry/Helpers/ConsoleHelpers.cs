8bda3083e3e4d5ef079c34dab1a6ce6e9a91c4b3
cpereira7 <cpereira7@users.noreply.github.com>
2025-02-13 20:16:16 +0000

    feat(Telemetry): Create Console Helpers to display progress status.
    - Created ConsoleHelpers.cs to display progress status.

---
+++ SampleStack.Telemetry/Helpers/ConsoleHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace SampleStack.Telemetry.Helpers
+{
+    internal static class ConsoleHelpers
+    {
+        public static void DisplayProgress(int current, int total)
+        {
+            int progressWidth = 50;
+            int progress = (int)((double)current / total * progressWidth);
+            
+            StringBuilder progressBar = new();
+            progressBar.Append('[');
+            progressBar.Append(new string('#', progress));
+            progressBar.Append(new string('-', progressWidth - progress));
+            progressBar.Append(']');
+            
+            Console.Write($"\rProgress: {progressBar} {current}/{total}");
+            if (current == total)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
